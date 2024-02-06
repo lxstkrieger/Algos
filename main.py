0aea3ad7f03d74a7c248c973ae640ae1aca700d2
Lxstkrieger <80554880+lxstkrieger@users.noreply.github.com>
2024-02-06 15:26:39 +0000

    sorting algo

--- main.py
+++ main.py
@@ -23,3 +23,16 @@ def selection_sort(arr):
 
 selection_sort(arr)
 print(arr)
+
+def insertion_sort(arr):
+    for i in range(1,len(arr)):
+        x = arr[i]
+        j = i-1
+
+        while j >= 0 and x > arr[j] :
+            arr[j+1] = arr[j]
+            j -=1
+        arr[j+1] = x
+
+insertion_sort(arr)
+print(arr) 
